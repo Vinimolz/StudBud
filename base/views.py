70e7caeaddb11e6817c45f15119d33602b25b07d
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-09 14:32:02 -0700

    Created first files and folders

---
+++ base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+# Create your views here.
+"""
+rooms = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Learn Java'},
+    {'id': 3, 'name': 'Learn C/C++'},
+]
+"""
+
+def home(request):
+    rooms = Room.objects.all()
+    content = {'rooms': rooms}
+    return render(request, 'base/home.html', content)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    
+    content = {'room': room}
+    return render(request, 'base/room.html', content)
