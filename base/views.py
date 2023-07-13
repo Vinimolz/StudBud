5052351b0df847683edd874537260d2689bbaa83
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-12 19:45:22 -0700

    Finished search functionality

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -15,11 +16,17 @@ def home(request):
 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) 
+    )
+
+    room_count = rooms.count()
 
     topics = Topic.objects.all()
 
-    content = {'rooms': rooms, 'topics': topics}
+    content = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', content)
 
 def room(request, pk):
