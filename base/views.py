0636f83360ce2661943fdf42ca18ee22c486b6a7
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-12 19:19:05 -0700

    Started implementing a search bar

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 """
@@ -12,8 +12,14 @@ rooms = [
 """
 
 def home(request):
-    rooms = Room.objects.all()
-    content = {'rooms': rooms}
+
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    content = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', content)
 
 def room(request, pk):
