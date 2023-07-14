898d7d751c0f6d3bccf3975bad05fc92e6128021
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-13 17:46:47 -0700

    Created login page and functionality

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -11,6 +14,28 @@ rooms = [
     {'id': 3, 'name': 'Learn C/C++'},
 ]
 """
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exists')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+    
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
 
