61e8ab54da2f71acbdf1c9b449669debaea3a051
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-15 17:18:16 -0700

    Finished CRUD func for chat rooms

--- base/urls.py
+++ base/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
     path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+
     path('login-page/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message')
 ]
--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # Create your views here.
 """
@@ -73,8 +73,6 @@ def register_user(request):
 
     return render(request, 'base/login_register.html', context)
 
-
-
 def home(request):
 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -94,8 +92,22 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    
-    content = {'room': room}
+    user_messages = room.message_set.all().order_by('-created')
+
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+
+        room.participants.add(request.user)
+
+        return redirect('room', pk=room.id)
+
+    content = {'room': room, 'user_messages': user_messages, 'participants': participants}
     return render(request, 'base/room.html', content)
 
 @login_required(login_url='login')
@@ -134,3 +146,16 @@ def delete_room(request, pk):
         return redirect('home')
     
     return render(request, 'base/delete_form.html', {'obj': room})
+
+@login_required(login_url='login')
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('This actions is not allowed')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete_form.html', {'obj': message})
